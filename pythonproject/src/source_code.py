6148b403969b9cfd5c5d5870551057d467199226
Rafael Davila Bugarin <rafaeldavila.b@gmail.com>
2023-09-07 19:39:17 +0000

    I added the data

--- pythonproject/src/source_code.py
+++ pythonproject/src/source_code.py
@@ -1,16 +1,18 @@
 """This script does not take any modules"""
 # test source code to make the python template worflow stable
 
+import os
+import pandas as pd
+import sys
 
-def add(arg_1, arg_2):
-    """add function which adds stuff"""
-    var1 = arg_1
-    var2 = arg_2
-    return var1 + var2
+def descriptive_statistics(data):    
+    print(data.describe())
 
 
-def main():
-    """Main process which calls add"""
-    test1 = 1
-    test2 = 2
-    return add(test1, test2)
+
+if __name__ =="__main__":
+    #current_dir = os.path.dirname(os.path.abspath(__file__))
+    #new_dir = current_dir + '/data'
+    #sys.path.insert(0, new_dir)
+    data=pd.read_csv("/workspaces/rd278-w2-pandas/pythonproject/src/data/food_data.csv")
+    data.describe()

7974e24d0f0dd0f65b58f1c43433910f20132c10
Rafael Davila Bugarin <rafaeldavila.b@gmail.com>
2023-09-07 19:48:56 +0000

    I changed the data

--- pythonproject/src/source_code.py
+++ pythonproject/src/source_code.py
@@ -6,7 +6,7 @@ import pandas as pd
 import sys
 
 def descriptive_statistics(data):    
-    print(data.describe())
+    return data.describe()
 
 
 
@@ -14,5 +14,5 @@ if __name__ =="__main__":
     #current_dir = os.path.dirname(os.path.abspath(__file__))
     #new_dir = current_dir + '/data'
     #sys.path.insert(0, new_dir)
-    data=pd.read_csv("/workspaces/rd278-w2-pandas/pythonproject/src/data/food_data.csv")
+    data=pd.read_csv("pythonproject/src/data/spotify.csv")
     data.describe()
